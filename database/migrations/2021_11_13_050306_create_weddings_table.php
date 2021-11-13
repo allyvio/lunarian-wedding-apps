774b11cc046d5827613c6625eaf2c024b45b77a4
Sasenna <lintangpanggalih@gmail.com>
2021-11-13 15:00:12 +0700

    move models to models dir

---
+++ database/migrations/2021_11_13_050306_create_weddings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeddingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weddings', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('calon_pria', 100);
+            $table->string('calon_wanita', 100);
+            $table->string('calon_pria_photo')->nullable();
+            $table->string('calon_wanita_photo')->nullable();
+            $table->enum('theme', ['default']);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weddings');
+    }
+}
