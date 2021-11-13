a836f6f06347866b5259010c5e5e3ff2bf162a49
Sasenna <lintangpanggalih@gmail.com>
2021-11-14 00:04:23 +0700

    add new 5 migrations and update dir structure

--- app/Models/Wedding.php
+++ app/Models/Wedding.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wedding extends Model
 {
-    //
+    protected $tables = 'weddings';
+    protected $fillable = [
+        'id',
+        'slug',
+        'title',
+        'description',
+        'calon_pria',
+        'calon_wanita',
+        'calon_pria_photo',
+        'calon_wanita_photo',
+        'theme'
+    ];
+    protected $attributes = [
+        'theme' => 'default',
+    ];
+
+    function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
---
+++ database/migrations/2021_11_13_164817_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invitation_id');
+            $table->text('message');
+            $table->boolean('is_show');
+            $table->timestamps();
+
+            $table->index(['invitation_id','is_show'],'comments_index');
+            $table->foreign('invitation_id')->references('id')->on('invitations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
