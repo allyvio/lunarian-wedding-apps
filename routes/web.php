da00ebf77385e515c4025c3a9de3845ca2bed686
Sasenna <lintangpanggalih@gmail.com>
2021-11-13 10:07:07 +0700

    adding default theme

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/default-theme', function () {
+    return view('themes.default.wrapper');
+});
