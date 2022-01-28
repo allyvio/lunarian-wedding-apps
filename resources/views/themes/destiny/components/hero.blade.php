a45d39b228cac2ad80086b40e328c30c2416b3ea
Mahrus13 <muhammadmahrus1501@gmail.com>
2022-01-28 14:26:42 +0700

    slider

--- resources/views/themes/destiny/components/hero.blade.php
+++ resources/views/themes/destiny/components/hero.blade.php
@@ -1,7 +1,47 @@
  <section class="hero overlay">
      <div class="background-img">
-         <img src="{{asset('assets/themes/destiny/img/hero-3.jpeg')}}" alt="">
+         <!-- <img src="{{asset('assets/themes/destiny/img/hero-3.jpeg')}}" alt=""> -->
      </div>
+     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+       <ol class="carousel-indicators">
+         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+         <!-- <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> -->
+       </ol>
+       <div class="carousel-inner">
+         <div class="carousel-item active">
+           <img class="d-block w-100" src="{{asset('assets\themes\ourlove\img\hero-3.jpeg')}}" alt="First slide">
+           <div class="carousel-caption d-none d-md-block">
+             <div class="hs-section-title">
+               <!-- <h1>Kita Akan Menikah</h1>
+               <div class="text-center">
+                   <h2 class="s2" style="font-style: italic; color:white;">{{$wedding->description ?? ''}}</h2>
+               </div> -->
+             </div>
+           </div>
+         </div>
+         <div class="carousel-item">
+           <img class="d-block w-100" src="{{asset('assets\themes\ourlove\img\hero-3.jpeg')}}" alt="First slide">
+           <div class="carousel-caption d-none d-md-block">
+             <div class="hs-section-title">
+               <!-- <h1>Kita Akan Menikah</h1>
+               <div class="text-center">
+                   <h2 class="s2" style="font-style: italic; color:white;">{{$wedding->description ?? ''}}</h2>
+               </div> -->
+             </div>
+           </div>
+         </div>
+       </div>
+       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="sr-only">Previous</span>
+       </a>
+       <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="sr-only">Next</span>
+       </a>
+     </div>
+
      <!--Inner hero-->
      <div class="inner-hero">
          <!--Container-->
